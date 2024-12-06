d39bcd1a190a1218eec349573c4ddb84ddad3eef
Ali <aliabbas808000@gmail.com>
2024-12-07 00:17:47 +0100

    add prep exercise js week3

---
+++ 1-JavaScript/Week3/prep-exercise/prep-exercise-js-w3.js
@@ -0,0 +1,15 @@
+import { modules, students, mentors, classes } from './hyf.js';
+
+const possibleMentorsForModule = (moduleName) => {
+  const module = modules.find((mod) => mod.name === moduleName);
+  if (!module) return [];
+  return mentors
+    .filter((mentor) => mentor.canTeach.includes(module.name))
+    .map((mentor) => mentor.name);
+};
+
+const findMentorForModule = (moduleName) => {
+  const possibleMentors = possibleMentorsForModule(moduleName);
+  if (possibleMentors.length === 0) return null;
+  return possibleMentors[Math.floor(Math.random() * possibleMentors.length)];
+};
